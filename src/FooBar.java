cdccf0744ca6b5e3b383e3d2380d564f7f40b98d
Ellie Rowe <elliejanerowe@gmail.com>
2019-09-03 11:32:37 +0100

    Initial commit

---
+++ src/FooBar.java
@@ -0,0 +1,28 @@
+public class FooBar {
+
+
+    public static String fooBar() {
+        int i;
+        String output = "";
+
+        for(i = 1; i <= 100; i++) {
+
+            if(i % 3 == 0 && i % 5 == 0) {
+                output += "foobar ";
+            }
+            else if(i % 3 == 0) {
+                output += "foo ";
+            }
+            else if(i % 5 == 0) {
+                output += "bar ";
+            }
+            else {
+                output += i + " ";
+            }
+
+        }
+
+        return output;
+    }
+
+}
